23a8114c64788a60a58221687ecaae70dd27f8a2
JuraganPacarCina <145534732+takumifahri@users.noreply.github.com>
2024-11-18 08:26:18 +0700

    change user and delete edit button

--- src/app/admin/user/page.jsx
+++ src/app/admin/user/page.jsx
@@ -555,6 +555,15 @@ export default function Home() {
                           placeholder="Username"
                           onChange={handleInputChange}
                         />
+                        <label className="mb-2 text-black float-start"> Avatar </label>
+                        <input
+                          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4"
+                          value={User.avatar}
+                          type="file"
+                          name="avatar"
+                          placeholder="Username"
+                          onChange={handleFileSelect}
+                        />
                         <label className="mb-2 text-black float-start"> Address </label>
                         <input
                           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 mb-4"
